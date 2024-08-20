ae12815de8ac14a986c754bd550bf05c0a037d89
Leo Espinoza <leo@holion.ai>
2024-08-20 19:40:43 +0000

    actualización a v4 de openai

--- app/api/code/route.ts
+++ app/api/code/route.ts
@@ -1,59 +1,59 @@
 import { auth } from "@clerk/nextjs";
 import { type NextRequest, NextResponse } from "next/server";
-//import { type ChatCompletionRequestMessage, Configuration,OpenAIApi,} from "openai";
-// New (i.e., OpenAI NodeJS SDK v4)
 import OpenAI from 'openai';
 
 import { increaseApiLimit, checkApiLimit } from "@/lib/api-limit";
 import { checkSubscription } from "@/lib/subscription";
 
+
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
-//const configuration = new Configuration({
-//  apiKey: process.env.OPENAI_API_KEY,
-//});
 
-//const openai = new OpenAIApi(configuration);
-
-const intructionMessage: ChatCompletionRequestMessage = {
+const instructionMessage: OpenAI.Chat.Completions.CreateChatCompletionRequestMessage = {
   role: "system",
-  content:
-    "You are a code generator. You must answer only in markdown code snippets. Use code comments for explaination.",
+  content: "You are a code generator. You must answer only in markdown code snippets. Use code comments for explanations."
 };
 
-export async function POST(req: NextRequest) {
+export async function POST(
+  req: Request
+) {
   try {
     const { userId } = auth();
-
     const body = await req.json();
-    const { messages } = body;
+    const { messages  } = body;
 
-    if (!userId) return new NextResponse("Unauthorized.", { status: 401 });
-    if (!configuration.apiKey)
-      return new NextResponse("OpenAI api key not configured.", {
-        status: 500,
-      });
+    if (!userId) {
+      return new NextResponse("No esta autorizado", { status: 401 });
+    }
 
-    if (!messages)
-      return new NextResponse("Messages are required.", { status: 400 });
+    if (!openai.apiKey) {
+      return new NextResponse("CLave de OpenAI no esta configurado.", { status: 500 });
+    }
+
+    if (!messages) {
+      return new NextResponse("Se requiere un mensaje", { status: 400 });
+    }
 
     const freeTrial = await checkApiLimit();
     const isPro = await checkSubscription();
 
-    if (!freeTrial && !isPro)
-      return new NextResponse("Free trial has expired.", { status: 403 });
+    if (!freeTrial && !isPro) {
+      return new NextResponse("La versión gratis ha finalizado. Por favor actualizar a Pro.", { status: 403 });
+    }
 
-    const response = await openai.createChatCompletion({
+    const response = await openai.chat.completions.create({
       model: "gpt-3.5-turbo",
-      messages: [intructionMessage, ...messages],
+      messages: [instructionMessage, ...messages]
     });
 
-    if (!isPro) await increaseApiLimit();
+    if (!isPro) {
+      await incrementApiLimit();
+    }
 
-    return NextResponse.json(response.data.choices[0].message, { status: 200 });
-  } catch (error: unknown) {
-    console.error("[CODE_ERROR]: ", error);
-    return new NextResponse("Internal server error.", { status: 500 });
+    return NextResponse.json(response.choices[0].message);
+  } catch (error) {
+    console.log('[CONVERSATION_ERROR]', error);
+    return new NextResponse('Error interno', { status: 500 });
   }
-}
+};
