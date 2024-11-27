b47c907df15a6ecdfc959ed0769d7bfea1045ebd
Leo Espinoza <leo@holion.ai>
2024-11-27 17:36:47 +0000

    manejo de menu

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -5,10 +5,14 @@ import { SideMessage } from "@/components/sidemessage";
 import { Sidebar } from "@/components/sidebar";
 import { getApiLimitCount } from "@/lib/api-limit";
 import { checkSubscription } from "@/lib/subscription";
+import { useRouter } from 'next/router';
 
 const DashboardLayout = async ({ children }: PropsWithChildren) => {
   const apiLimitCount = await getApiLimitCount();
   const isPro = await checkSubscription();
+  const router = useRouter();
+   // Aquí definimos la condición para la página específica
+   const isSpecificPage = router.asPath === "/conversation"; // Asegúrate de usar el path correcto
 
   return (
     // <div className="h-full relative">
@@ -23,22 +27,32 @@ const DashboardLayout = async ({ children }: PropsWithChildren) => {
     //   </main>
     // </div>
 
-    <div className="h-full flex relative">
-        {/* Barra lateral izquierda */}
-        <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 bg-gray-900">
-          <Sidebar apiLimitCount={apiLimitCount} isPro={isPro} />
-        </div>
-        {/* Barra lateral derecha */}
-        <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 right-0 bg-gray-100">
-          <SideMessage apiLimitCount={apiLimitCount} isPro={isPro} />
-        </div>
-        {/* Contenido principal */}
-        <main className="flex-1 md:ml-72 md:mr-72">
-          <Navbar />
-          {children}
-        </main>
+    <div className="h-full relative">
+      {/* Barra lateral izquierda */}
+      <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 bg-gray-900">
+        <Sidebar apiLimitCount={apiLimitCount} isPro={isPro} />
+      </div>
 
+      {/* Contenido principal */}
+      <main className="flex-1 md:ml-72">
+        <Navbar />
+        {isSpecificPage ? (
+          <div className="flex">
+            {/* Contenido principal */}
+            <div className="flex-1">
+              {children}
+            </div>
 
+            {/* Menú lateral específico */}
+            <div className="hidden lg:flex lg:w-72 lg:flex-col bg-gray-100 p-4 border-l">
+              <SideMessage apiLimitCount={apiLimitCount} isPro={isPro} />
+            </div>
+          </div>
+        ) : (
+          // Renderiza children normalmente para otras páginas
+          <>{children}</>
+        )}
+      </main>
     </div>
   );
 };
