dca13b3910ca41f5c1ca0aab0fcc99bb05d8ad2a
Leo Espinoza <leo@holion.ai>
2024-11-27 16:53:26 +0000

    agregación de barra lateral

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -11,16 +11,34 @@ const DashboardLayout = async ({ children }: PropsWithChildren) => {
   const isPro = await checkSubscription();
 
   return (
-    <div className="h-full relative">
-      <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 bg-gray-900">
-        <Sidebar apiLimitCount={apiLimitCount} isPro={isPro} />
-      </div>
+    // <div className="h-full relative">
+    //   <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 bg-gray-900">
+    //     <Sidebar apiLimitCount={apiLimitCount} isPro={isPro} />
+    //   </div>
 
-      <main className="md:md:pl-72">
-        <Navbar />
-        {children}
-        <SideMessage apiLimitCount={apiLimitCount} isPro={isPro} />
-      </main>
+    //   <main className="md:md:pl-72">
+    //     <Navbar />
+    //     {children}
+    //     <SideMessage apiLimitCount={apiLimitCount} isPro={isPro} />
+    //   </main>
+    // </div>
+
+    <div className="h-full flex relative">
+        {/* Barra lateral izquierda */}
+        <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 bg-gray-900">
+          <Sidebar apiLimitCount={apiLimitCount} isPro={isPro} />
+        </div>
+
+        {/* Contenido principal */}
+        <main className="flex-1 md:ml-72 md:mr-72">
+          <Navbar />
+          {children}
+        </main>
+
+        {/* Barra lateral derecha */}
+        <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 right-0 bg-gray-100">
+          <SideMessage apiLimitCount={apiLimitCount} isPro={isPro} />
+        </div>
     </div>
   );
 };
